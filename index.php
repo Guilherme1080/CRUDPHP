b37c145dc84fe51145c496ffd300cc6ed291d702
Guilherme De Miranda Cardoso <guilhermemiranda1080@gmail.com>
2025-01-15 10:15:37 -0400

    CRUDPHP
    CRUDPHP

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+require './Entity/Cliente.php';
+
+
+//ESSAS VARIÁVEIS IRAO ARMAZENAR AS INFORMAÇOES VINDAS DO HTML, POR EXEMPLO
+//PREENCHIMENTO DE UM FORMULÁRIO, TRATANDO
+$nome = "MARCOS TAKARO";
+$cpf = "45646546";
+$email = "kaoro@takaro.com";
+
+//INSTANCIANDO UM OBJETO DO TIPO CLIENTE, UM CLIENTE TEM ATRIBUTOS NOME,CPF,EMAIL
+$cli = new Cliente($nome,$cpf,$email);
+
+//CHAMA O MÉTODO PARA CADASTRAR ESSE CLIENTE QUE FOI INSTANCIADO
+$cli->cadastrar();
+
+//RESULT VARIÁVEL QUE CONTÉM UM ARRAY RETORNADO DO BANCO DE DADOS
+$result = $cli->buscar();
+
+foreach ($result as $pessoa){
+    echo "<br> " .$pessoa['id'] . ' ' .$pessoa['nome'] ;
+}
